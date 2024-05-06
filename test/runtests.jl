07a92e1c6ac18a6302fab7d312fe06d74e0b932b
Matheus Nogueira <matnogueira@gmail.com>
2024-05-06 10:18:00 -0300

    minor change in runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,11 +5,11 @@ using JSON3, CSV, DataFrames, StateSpaceModels
 
 
 # Include all test files
+include("test_fit.jl")
 include("test_components_dynamics.jl")
 include("test_optimization.jl")
 include("test_forecast.jl")
 include("test_distributions.jl")
 include("test_initialization.jl")
-include("test_fit.jl")
 
 
