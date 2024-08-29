6b49e318a662b163e6d2509fb6f8f10d682d69d0
matheusaps <matheus03alves@gmail.com>
2024-08-29 12:23:22 -0300

    implicit formulation updates and preliminary results

--- test_implicit_gas.jl
+++ test_implicit_gas.jl
@@ -74,35 +74,86 @@ function implicit_local_level_gas(y::Vector{Float64}, λ::Float64)
     model = Model(Ipopt.Optimizer)
     set_silent(model)
     set_optimizer_attribute(model, "max_iter", 60000)
-    set_optimizer_attribute(model, "max_cpu_time", 100.0)
+    set_optimizer_attribute(model, "max_cpu_time", 120.0)
     set_optimizer_attribute(model, "tol",  0.0005) 
 
     @variable(model, m_update[1:T])
     @variable(model, σ  ≥ 0.0004)
+    @variable(model, m_pred1)
     @variable(model, s[1:T])
-
     @variable(model, -2 ≤ κ ≤ 2)
 
     #passo de previsão
-    m_pred = Vector(undef, T)
+    m_pred    = Vector(undef, T)
+    m_pred[1] = m_pred1
+    #s         = Vector(undef, T)
+    #s[1]      = m_update[1] - m_pred[1]
     for t in 2:T
-        m_pred[t] = m_update[t-1]  + κ * s[t-1]
+        m_pred[t] = m_update[t-1]
+        #s[t]      = m_update[t] - m_pred[t]
     end
 
     @expression(model, μ, m_update)
-    @constraint(model,[t = 2:T], s[t] == m_update[t] - m_pred[t])
-   
+    @constraint(model, [t = 1:T], m_update[t] == m_pred[t] + κ*s[t])
+    
     f(y,μ, σ) = logpdf(Normal(μ,σ),y)
     @operator(model,ℓ,3,f)
-    @objective(model, Max, sum(ℓ.(y,μ, σ)) - λ*s'I(T)*s)
+    @objective(model, Max, sum(ℓ(y[t],μ[t], σ) for t = 1:T) - (λ/2)*sum(s[t]^2 for t = 1:T))
     
     set_start_value.(m_update, y)
+    #set_start_value(m_pred1, y[1])
     set_start_value(σ, std(diff(y)))
+    set_start_value(κ, 0.02)
 
     optimize!(model)
-
     println(termination_status(model))
-    return value.(m_update), value.(m_pred[2:end]), value(σ), value.(s), value(κ)
+
+    return value.(m_update), value.(m_pred), value(σ), value.(s), value(κ)
+    
+end
+
+function implicit_local_level_gas_v2(y::Vector{Float64}, λ::Float64, d::Float64)
+    T = length(y)
+
+    model = Model(Ipopt.Optimizer)
+    set_silent(model)
+    set_optimizer_attribute(model, "max_iter", 100000)
+    set_optimizer_attribute(model, "max_cpu_time", 180.0)
+    set_optimizer_attribute(model, "tol",  0.0005) 
+
+    @variable(model, m_update[1:T])
+    @variable(model, σ  ≥ 0.0004)
+    @variable(model, m_pred1)
+    @variable(model, -2 ≤ κ ≤ 2)
+
+    #passo de previsão
+    m_pred    = Vector(undef, T)
+    m_pred[1] = m_pred1
+    s         = Vector(undef, T)
+    s[1]      = m_update[1] - m_pred[1]
+    for t in 2:T
+        m_pred[t] = m_update[t-1] #+ κ*s[t-1]
+        s[t]      = m_update[t] - m_pred[t]
+    end
+
+    P = ones(T) * (1 / (λ * κ)) * (1/σ^2)^(d)
+
+    #@expression(model, μ, m_update)
+    #@constraint(model, [t = 1:T], m_update[t] == m_pred[t] + κ*s[t])
+    
+    f(y,μ, σ) = logpdf(Normal(μ,σ),y)
+    @operator(model,ℓ,3,f)
+    #@objective(model, Max, sum(ℓ(y[t],m_update[t], σ) for t = 1:T) - (λ/2)*sum(P[t]*s[t]^2 for t = 1:T))
+    @objective(model, Max, sum(ℓ.(y,m_update, σ)) - (λ/2)*s'diagm(P)*s)
+    
+    set_start_value.(m_update, y)
+    #set_start_value(m_pred1, y[1])
+    set_start_value(σ, std(diff(y)))
+    set_start_value(κ, 0.02)
+
+    optimize!(model)
+    println(termination_status(model))
+    return value.(m_update), value.(m_pred), value(σ), value.(s), value(κ), value.(P)
     
 end
 
@@ -157,6 +208,7 @@ function implicit_local_linear_trend_gas(y::Vector{Float64}, λ::Float64)
     
 end
 
+
 # Parâmetros
 T = 100 
 N = 10
@@ -166,47 +218,83 @@ N = 10
 # Simulação
 series = simulate_local_level(T, N, σ²_μ, σ²_ε)
 
-y = series[:, 1]
+y = series[:, 6]
 plot(y)
+d_values = [0.0, 0.5, 1.0]
 
 dict_results = Dict()
-for λ in 0:0.1:1
-    dict_results[λ] = Dict()
-    μ_up, μ_pred, σ, s, κ = implicit_local_level_gas(y, λ);
-    score_est = (y .- μ_up) ./ σ^2 #score da normal calculado com a média atualizada
-    score_alt = 2* λ .* (μ_up[2:end] .- μ_pred) 
+for i in 1:N
+    println("serie $i")
+    y = series[:, i]
+    graficos_fit = Vector(undef, 3)
+    graficos_score = Vector(undef, 3)
+    for j in 1:3
+        try
+            d = d_values[j]
+            println("d = $d")
+            λ = 0.2
+            dict_results[i] = Dict()
+            μ_up, μ_pred, σ, s, κ, P_opt = implicit_local_level_gas_v2(y, λ,d);
+           
+            dist = UnobservedComponentsGAS.NormalDistribution()
+            model = UnobservedComponentsGAS.GASModel(dist, [true, false], d, "random walk", "", missing)
+            fitted_model = UnobservedComponentsGAS.fit(model, y)
 
-    dist = UnobservedComponentsGAS.NormalDistribution()
-    model = UnobservedComponentsGAS.GASModel(dist, [true, false], 0.0, "random walk slope", "", missing)
-    fitted_model = UnobservedComponentsGAS.fit(model, y)
+            if d == 0
+                coef_score = (λ * P_opt)[1]
+                score_pack = (y .- fitted_model.fitted_params["param_1"]) ./ fitted_model.fitted_params["param_2"][1]
+                score_est = (y .- μ_up) ./ σ^2 #score da normal calculado com a média atualizada
+    
+            elseif d == 0.5
+                coef_score = (λ * P_opt * σ)[1]
+                score_pack = (y .- fitted_model.fitted_params["param_1"]) ./ sqrt(fitted_model.fitted_params["param_2"][1])
+                score_est = (y .- μ_up) ./ σ #score da normal calculado com a média atualizada
+    
+            else
+                coef_score = (λ * P_opt * σ^2)[1]
+                score_pack = (y .- fitted_model.fitted_params["param_1"])
+                score_est = (y .- μ_up)
+    
+            end
 
-    score_pack = (y .- fitted_model.fitted_params["param_1"]) ./ fitted_model.fitted_params["param_2"][1]
+            dict_results[i]["μ_pred"] = μ_pred
+            dict_results[i]["μ_up"] = μ_up
+            dict_results[i]["σ2"] = σ^2
+            dict_results[i]["s"] = s[2:end]
+            dict_results[i]["κ"] = κ
+            dict_results[i]["score_est"] = score_est[2:end]
+            #dict_results[i]["score_alt"] = score_alt[2:end]
+            dict_results[i]["score_pack"] = score_pack[2:end]
+            dict_results[i]["σ2_pack"] = fitted_model.fitted_params["param_2"][1]
+            dict_results[i]["μ_pack"] = fitted_model.fitted_params["param_1"]
 
-    dict_results[λ]["μ_pred"] = μ_up
-    dict_results[λ]["μ_up"] = μ_pred
-    dict_results[λ]["σ2"] = σ^2
-    dict_results[λ]["s"] = s
-    dict_results[λ]["κ"] = κ
-    dict_results[λ]["score_est"] = score_est[2:end]
-    dict_results[λ]["score_alt"] = score_alt[2:end]
-    dict_results[λ]["score_pack"] = score_pack[2:end]
-    dict_results[λ]["σ2_pack"] = fitted_model.fitted_params["param_2"][1]
-    dict_results[λ]["μ_pack"] = fitted_model.fitted_params["param_1"]
+            graficos_fit[j] = plot(y[3:end], label = "série", color = "black", linewidth = 1.5)
+            plot!(dict_results[i]["μ_pred"][3:end], label = "média preditiva", color = "red", linewidth = 1.0)
+            plot!(dict_results[i]["μ_up"][3:end], label = "média atualizada", color = "blue", linewidth = 1.0) #atualizado mais suave do que o predito, o que faz sentido
+            plot!(dict_results[i]["μ_pack"][3:end], label = "média pacote", color = "green", linewidth = 1.0, legend =:bottomleft,  legendfontsize=5)
+            title!("d = $d")
+            #savefig("results_implicit_gas/medias_serie$i.png")
 
-    plot(y[2:end], label = "série", color = "black")
-    plot!(dict_results[λ]["μ_pred"], label = "média preditiva", color = "red")
-    plot!(dict_results[λ]["μ_up"][2:end], label = "média atualizada", color = "blue") #atualizado mais suave do que o predito, o que faz sentido
-    plot!(dict_results[λ]["μ_pack"], label = "média pacote", color = "green")
-    title!("λ = $λ")
-    savefig("results_implicit_gas/medias_ll_lambda_$(λ).png")
+            graficos_score[j] = plot(dict_results[i]["s"] .* coef_score, label = "score_implicit")
+            plot!(dict_results[i]["score_est"], label = "score_est")
+            #plot!(dict_results[λ]["s"], label = "score_opt")
+            plot!(dict_results[i]["score_pack"], label = "score_pack", legend =:bottomleft, legendfontsize=8)
+            title!("d = $d")
+            #savefig("results_implicit_gas/scores_serie$i.png")
+        catch
+            println("Erro na serie $i")
+        end
+    end
+    plot(graficos_fit[1], graficos_fit[2], graficos_fit[3], layout=(2, 2), suptitle = "Série $i")
+    savefig("results_implicit_gas/medias_serie$i.png")
 
-    plot(dict_results[λ]["score_est"][2:end], label = "score_est")
-    plot!(dict_results[λ]["score_alt"], label = "score_alt")
-    # plot!(dict_results[λ]["s"], label = "score_opt")
-    # plot!(dict_results[λ]["score_pack"], label = "score_pack")
-    title!("Scores λ = $λ")
-    savefig("results_implicit_gas/scores_ll_lambda_$(λ).png")
+    plot(graficos_score[1], graficos_score[2], graficos_score[3], layout=(2, 2), suptitle = "Série $i")
+    savefig("results_implicit_gas/scores_serie$i.png")
 end
+
+
+
+
 
 # O nosso pacote explode muito a variância
 for λ in 0:0.1:1
@@ -279,3 +367,4 @@ value(ω)
 plot(y[3:end])
 plot!(value.(μ)[3:end])
 plot!(value.(m_pred[3:end]))
+
